b43482ed0d8a48f0453246110efd840d8c286ca4
Emanuele Cordano <emanuele.cordano@gmail.com>
2023-08-22 15:13:52 +0200

    1.0.6

--- R/boussinesq-package.R
+++ R/boussinesq-package.R
@@ -7,18 +7,6 @@
 #' 
 #' Analytic solutions for (ground-water) Boussinesq Equation 
 #'
-#' \tabular{ll}{
-#' Package: \tab boussinesq\cr
-#' Type: \tab Package\cr
-#' Version: \tab 1.0.2\cr
-#' Date: \tab   2013-04-18\cr
-#' License: \tab GPL (>= 2)\cr
-#' LazyLoad: \tab yes\cr
-#' Depends: \tab R(>=2.12)\cr
-#' }
-#'
-#' 
-#' 
 #'
 #' @name boussinesq-package
 #' @aliases boussinesq
